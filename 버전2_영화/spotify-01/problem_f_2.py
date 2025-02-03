b96cd1c96d52d641e44cbf9ff1acb53bab4a0415
daye <myannnng@gmail.com>
2025-02-03 17:52:54 +0900

    problem_f_2까지 품

--- 버전2_영화/spotify-01/problem_f_2.py
+++ 버전2_영화/spotify-01/problem_f_2.py
@@ -6,12 +6,58 @@ import json
 from pprint import pprint
 from pathlib import Path
 
+
+
 current_dir = Path(__file__).resolve().parent
+
+artist_json = open(current_dir / 'data' / 'artists.json', encoding='utf-8')
+artists_list = json.load(artist_json)
+
+genres_json = open(current_dir / 'data' / 'genres.json', encoding='utf-8')
+genres_list = json.load(genres_json)
+
+
+def artists_info(get):
+
+    id_list = []
+    
+    for individual_information in get:
+            
+            id_list.append(individual_information['id'])
+    
+    return id_list
+
+
+def genres_info():
+    acoustic_id = None 
+    for i in genres_list:
+          if i['name'] == 'acoustic':
+               acoustic_id = i['id']
+    return acoustic_id          
+
 
 
 def acoustic_artists():
-    # 여기에 코드를 작성합니다.
-    pass
+
+    artists = []
+
+    acoustic_id = genres_info()
+
+
+    numbers = artists_info(artists_list)
+
+
+    for i in numbers:
+
+            artist_folder_json = open(current_dir / 'data' / 'artists'/ f'{i}.json', encoding='utf-8')
+            artists_folder_list = json.load(artist_folder_json)
+
+            for nums in artists_folder_list['genres_ids']:
+                 if nums == acoustic_id:
+                      artists.append(artists_folder_list['name'])
+
+    return artists
+    
 
 
 # 아래의 코드는 수정하지 않습니다.
